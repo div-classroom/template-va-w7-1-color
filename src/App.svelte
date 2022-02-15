2fe42ee4f07799b76dfeea72cce12350d1f078f0
Minsuk Kahng <minsukkahng@users.noreply.github.com>
2022-02-15 14:10:38 -0800

    Add files

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import StockLineChart from "./components/StockLineChart.svelte"
+	import PopulationMap from "./components/PopulationMap.svelte"	
+</script>
+	
+<div id="container">
+	<StockLineChart />
+	<PopulationMap />
+</div>
+	
+<style>
+	#container {
+		display: flex;
+	}
+</style>
